0c21ec921090c269cb62e5574405167adc6df517
Joao Felipe <joaofelipe1294@gmail.com>
2021-08-01 17:24:14 -0300

    feat: generated form_kind model.

---
+++ db/migrate/20210801202327_create_aact_form_engine_form_kinds.rb
@@ -0,0 +1,11 @@
+class CreateAactFormEngineFormKinds < ActiveRecord::Migration[6.1]
+  def change
+    create_table :aact_form_engine_form_kinds, id: :uuid do |t|
+      t.string :name
+      t.string :description
+      t.integer :answer_sequence
+
+      t.timestamps
+    end
+  end
+end
