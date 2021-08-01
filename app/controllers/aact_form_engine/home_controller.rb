756b2319053ddd1112e7cdaf2e7f4f30626f75fb
Joao Felipe <joaofelipe1294@gmail.com>
2021-08-01 20:41:29 -0300

    feat: add home_controller.

---
+++ app/controllers/aact_form_engine/home_controller.rb
@@ -0,0 +1,8 @@
+require_dependency "aact_form_engine/application_controller"
+
+module AactFormEngine
+  class HomeController < ApplicationController
+    def index
+    end
+  end
+end
--- dummy_app/config/routes.rb
+++ dummy_app/config/routes.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  mount AactFormEngine::Engine => "/aact_form_engine"
+  mount AactFormEngine::Engine => '/aact_form_engine'
+
+  root to: 'aact_form_engine/home#index'
 end
