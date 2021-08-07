64e9dc9d765f349cb55c3f2b37b98655c5164798
Joao Felipe <joaofelipe1294@gmail.com>
2021-08-07 10:02:11 -0300

    feat: add index route.

--- app/controllers/aact_form_engine/home_controller.rb
+++ app/controllers/aact_form_engine/home_controller.rb
@@ -1,8 +1,14 @@
-require_dependency "aact_form_engine/application_controller"
+# frozen_string_literal: true
+
+require_dependency 'aact_form_engine/application_controller'
 
 module AactFormEngine
   class HomeController < ApplicationController
-    def index
-    end
+    def index; end
   end
 end
+
+
+# todo
+#    - adicionar devise no dummy app
+#    - estilizar home
